c2bc10023c879aac890aed0bd51bcd207679aab0
DiniBis <106832577+DiniBis@users.noreply.github.com>
2024-02-04 12:20:05 +0100

    Mise à jour sso.js: retrait des affichages de films
    Déplacés dans index.js

--- sso.js
+++ sso.js
@@ -27,7 +27,6 @@ window.onload = async () => {
     }  
 }
 
-
 // Cette fonction verifie qu'un token existe et redirige l'utilisateur vers le lien tmdb pour valider le token
 async function redirectUserToSSO() {
     let tokenData = await getNewTMDBToken()
@@ -105,41 +104,3 @@ function deconnexionaffiche() {
     deco.addEventListener('click',()=> hideloginbutton())
 }
 deconnexionaffiche()
-
-//requête, films en tendance
-async function getMovie(){
-    const options = {
-        method: 'GET',
-        headers: {
-          accept: 'application/json',
-          Authorization: `Bearer ${ssoTmdbReadApiKey}`
-        }
-      };
-      let responseDetail = await fetch('https://api.themoviedb.org/3/movie/popular?language=fr-FR&page=1', options).catch(err => console.error(err));
-      if(!responseDetail){
-          return
-      }
-      let detailData = await responseDetail.json();
-      console.log(detailData);
-      return detailData;
-}
-
-//affichage des films, ajout à l'html
-async function displayMovie(){
-    let movies = await getMovie()
-    results=movies.results
-    for (let i=0;i<results.length;i++){ //Parcourir tous les résultats   
-        let filmsContainer = document.createElement('div')
-        filmsContainer.innerHTML = `
-        <h3>${results[i].original_title}</h3>
-        <img src=https://image.tmdb.org/t/p/w500${results[i].poster_path} alt="poster du film ${results[i].original_title}"/>
-        <p>${results[i].release_date}</p>
-        `//Affiche dans l'ordre: le titre, l'image du film et sa date de sortie
-        filmsContainer.addEventListener('click',function(){
-            window.location.href=`movie.html?${results[i].id}`;
-        }) //Au clique, redirige vers movie.html du film correspondant
-        let filmsDiv = document.getElementById('films');
-        filmsDiv.appendChild(filmsContainer)
-    }
-}
-displayMovie()
